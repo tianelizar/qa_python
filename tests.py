306458e7553ace60788d24c10422c8e5e5976f1b
Tatiana Elizarova <tiana.elizar@yandex.ru>
2025-08-07 13:50:12 +0400

    new tests added

--- tests.py
+++ tests.py
@@ -30,30 +30,44 @@ class TestBooksCollector:
         books = collector.get_books_genre()
         assert 'Как поймать монстра' in books and len(books) == 1
 
-    # 3 проверка установления жанра
-    # заодно проверяется получение жанра по имени
-
+    # 3 проверка установления жанра - поменяла на прямое обращение к словарю
+    
     def test_set_book_genre_horror_set(self, collector):
         collector.add_new_book('Гордость и Предубеждение и Зомби')
         collector.set_book_genre('Гордость и Предубеждение и Зомби', 'Ужасы')
-        assert collector.get_book_genre('Гордость и Предубеждение и Зомби') == 'Ужасы'
+        assert collector.books_genre['Гордость и Предубеждение и Зомби'] == 'Ужасы'
 
-    # 4 установление жанра для несуществующей книги
+    # 4 проверка получения жанра по имени - добавила отдельную проверку
+
+    def test_get_book_genre_existing_book_show(self, collector):
+        collector.add_new_book('Гордость и Предубеждение и Зомби')
+        collector.set_book_genre('Гордость и Предубеждение и Зомби', 'Ужасы')
+        genre = collector.get_book_genre('Гордость и Предубеждение и Зомби')
+        assert genre == 'Ужасы'
+
+    #5 проверка получения жанра, если жанр не установлен - добавила отдельную проверку
+
+    def test_get_book_genre_no_genre_empty(self, collector):
+        collector.add_new_book('Гордость и Предубеждение и Зомби')
+        genre = collector.get_book_genre('Гордость и Предубеждение и Зомби')
+        assert genre == ''
+
+    # 6 установление жанра для несуществующей книги
 
     def test_set_book_genre_invalid_book_not_set(self, collector):
         collector.set_book_genre('Кармилла', 'Ужасы')
         assert collector.get_book_genre('Кармилла') is None
 
-    # 5 некорректные жанры
+    # 7 некорректные жанры - поменяла на прямое обращение к словарю
 
     @pytest.mark.parametrize('invalid_genre', ['', 'Классика'])
 
     def test_set_book_genre_invalid_genre_not_set(self, collector, invalid_genre):
         collector.add_new_book('Улисс')
         collector.set_book_genre('Улисс', invalid_genre)
-        assert collector.get_book_genre('Улисс') == ''
+        assert collector.books_genre['Улисс'] == ''
 
-    # 6 список книг по жанру
+    # 8 список книг по жанру
 
     def test_get_books_with_specific_genre_horror_show_horror(self, collector):
         for book in ['Оно', 'Интервью с вампиром', 'Человек-невидимка']:
@@ -65,7 +79,8 @@ class TestBooksCollector:
         horror_books = collector.get_books_with_specific_genre('Ужасы')
         assert 'Оно' in horror_books and 'Интервью с вампиром' in horror_books and len(horror_books) == 2
 
-    # 7 детские книги
+    # 9 детские книги
+
     def test_get_books_for_children_horror_and_cartoons_show_cartoons(self, collector):
         collector.add_new_book('Оно')
         collector.add_new_book('Лунтик')
@@ -73,38 +88,54 @@ class TestBooksCollector:
         collector.set_book_genre('Лунтик', 'Мультфильмы')
         assert collector.get_books_for_children() == ['Лунтик']
 
-    # 8 добавление в избранное + список избранного
+    # 10 добавление в избранное - поменяла на прямое обращение к списку
 
     def test_add_book_in_favorites_one_book_added(self, collector):
         collector.add_new_book('Оно')
         collector.add_book_in_favorites('Оно')
-        assert 'Оно' in collector.get_list_of_favorites_books()
+        assert 'Оно' in collector.favorites
 
-    #9 удаление из избранного
+    # 11 удаление из избранного - поменяла на прямое обращение к списку
 
     def test_delete_book_from_favorites_existing_book_deleted(self, collector):
         collector.add_new_book('Оно')
         collector.add_book_in_favorites('Оно')
         collector.delete_book_from_favorites('Оно')
-        assert 'Оно' not in collector.get_list_of_favorites_books()
+        assert 'Оно' not in collector.favorites
 
-    #10 добавление дубликата в избранное
+    # 12 добавление дубликата в избранное - поменяла на прямое обращение к списку
 
     def test_add_book_in_favorites_dupe_not_added(self, collector):
         collector.add_new_book('Оно')
         collector.add_book_in_favorites('Оно')
         collector.add_book_in_favorites('Оно')
-        assert len(collector.get_list_of_favorites_books()) == 1
+        assert len(collector.favorites) == 1
 
-    #11 позитивная проверка get_books_genre
+    #13 позитивная проверка get_books_genre
 
     def test_get_books_genre_one_book_show(self, collector):
         collector.add_new_book('Оно')
         collector.set_book_genre('Оно', 'Ужасы')
         assert collector.get_books_genre() == {'Оно': 'Ужасы'}
 
-    #12 добавление в избранное книги не из коллекции
+    #14 добавление в избранное книги не из коллекции - поменяла на прямое обращение к списку
 
     def test_add_book_in_favorites_book_not_in_collection_not_added(self, collector):
         collector.add_book_in_favorites('Оно')
-        assert 'Оно' not in collector.get_list_of_favorites_books()
+        assert 'Оно' not in collector.favorites
+
+    # 15 получение списка избранного - добавила отдельную проверку
+
+    def test_get_list_of_favorites_books_three_books_show(self, collector):
+        books = ['Оно', 'Интервью с вампиром', 'Человек-невидимка']
+        for book in books:
+            collector.add_new_book(book)
+            collector.add_book_in_favorites(book)
+        favorite = collector.get_list_of_favorites_books()
+        assert set(favorite) == set(books)
+
+    #16 получение пустого списка избранного - добавила отдельную проверку
+
+    def test_get_list_of_favorites_books_no_books_empty_list(self, collector):
+        collector.add_new_book('Оно')
+        assert collector.get_list_of_favorites_books() == []
